f233f47690125abb97f1c4cdf9966b17b4653b78
Bohdan <zikbert77@gmail.com>
2025-11-14 15:52:43 +0100

    Add order model

---
+++ src/Entity/Order.php
@@ -0,0 +1,297 @@
+<?php
+
+namespace App\Entity;
+
+use Andante\TimestampableBundle\Timestampable\TimestampableInterface;
+use Andante\TimestampableBundle\Timestampable\TimestampableTrait;
+use App\Entity\Enum\OrderStatus;
+use App\Repository\OrderRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order implements TimestampableInterface
+{
+    use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $uid = null;
+
+    #[ORM\ManyToOne]
+    private ?User $relatedUser = null;
+
+    #[ORM\Column(enumType: OrderStatus::class)]
+    private ?OrderStatus $status = null;
+
+    #[ORM\Column]
+    private ?int $subtotalAmount = null;
+
+    #[ORM\Column]
+    private ?int $discountAmount = null;
+
+    #[ORM\Column]
+    private ?int $shippingCostAmount = null;
+
+    #[ORM\Column]
+    private ?int $taxAmount = null;
+
+    #[ORM\Column]
+    private ?int $totalAmount = null;
+
+    #[ORM\Column]
+    private ?int $paidAmount = null;
+
+    #[ORM\Column(length: 120, nullable: true)]
+    private ?string $customerFirstName = null;
+
+    #[ORM\Column(length: 120, nullable: true)]
+    private ?string $customerLastName = null;
+
+    #[ORM\Column(length: 120, nullable: true)]
+    private ?string $customerEmail = null;
+
+    #[ORM\Column(length: 60, nullable: true)]
+    private ?string $customerPhone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $shippingAddress = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $paidAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $shippedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $completedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $canceledAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUid(): ?string
+    {
+        return $this->uid;
+    }
+
+    public function setUid(string $uid): static
+    {
+        $this->uid = $uid;
+
+        return $this;
+    }
+
+    public function getRelatedUser(): ?User
+    {
+        return $this->relatedUser;
+    }
+
+    public function setRelatedUser(?User $relatedUser): static
+    {
+        $this->relatedUser = $relatedUser;
+
+        return $this;
+    }
+
+    public function getStatus(): ?OrderStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(OrderStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getSubtotalAmount(): ?int
+    {
+        return $this->subtotalAmount;
+    }
+
+    public function setSubtotalAmount(int $subtotalAmount): static
+    {
+        $this->subtotalAmount = $subtotalAmount;
+
+        return $this;
+    }
+
+    public function getDiscountAmount(): ?int
+    {
+        return $this->discountAmount;
+    }
+
+    public function setDiscountAmount(int $discountAmount): static
+    {
+        $this->discountAmount = $discountAmount;
+
+        return $this;
+    }
+
+    public function getShippingCostAmount(): ?int
+    {
+        return $this->shippingCostAmount;
+    }
+
+    public function setShippingCost(int $shippingCostAmount): static
+    {
+        $this->shippingCostAmount = $shippingCostAmount;
+
+        return $this;
+    }
+
+    public function getTaxAmount(): ?int
+    {
+        return $this->taxAmount;
+    }
+
+    public function setTax(int $taxAmount): static
+    {
+        $this->taxAmount = $taxAmount;
+
+        return $this;
+    }
+
+    public function getTotalAmount(): ?int
+    {
+        return $this->totalAmount;
+    }
+
+    public function setTotalAmount(int $totalAmount): static
+    {
+        $this->totalAmount = $totalAmount;
+
+        return $this;
+    }
+
+    public function getPaidAmount(): ?int
+    {
+        return $this->paidAmount;
+    }
+
+    public function setPaidAmount(int $paidAmount): static
+    {
+        $this->paidAmount = $paidAmount;
+
+        return $this;
+    }
+
+    public function getCustomerFirstName(): ?string
+    {
+        return $this->customerFirstName;
+    }
+
+    public function setCustomerFirstName(?string $customerFirstName): static
+    {
+        $this->customerFirstName = $customerFirstName;
+
+        return $this;
+    }
+
+    public function getCustomerLastName(): ?string
+    {
+        return $this->customerLastName;
+    }
+
+    public function setCustomerLastName(?string $customerLastName): static
+    {
+        $this->customerLastName = $customerLastName;
+
+        return $this;
+    }
+
+    public function getCustomerEmail(): ?string
+    {
+        return $this->customerEmail;
+    }
+
+    public function setCustomerEmail(?string $customerEmail): static
+    {
+        $this->customerEmail = $customerEmail;
+
+        return $this;
+    }
+
+    public function getCustomerPhone(): ?string
+    {
+        return $this->customerPhone;
+    }
+
+    public function setCustomerPhone(?string $customerPhone): static
+    {
+        $this->customerPhone = $customerPhone;
+
+        return $this;
+    }
+
+    public function getShippingAddress(): ?string
+    {
+        return $this->shippingAddress;
+    }
+
+    public function setShippingAddress(string $shippingAddress): static
+    {
+        $this->shippingAddress = $shippingAddress;
+
+        return $this;
+    }
+
+    public function getPaidAt(): ?DateTimeImmutable
+    {
+        return $this->paidAt;
+    }
+
+    public function setPaidAt(DateTimeImmutable $paidAt): static
+    {
+        $this->paidAt = $paidAt;
+
+        return $this;
+    }
+
+    public function getShippedAt(): ?DateTimeImmutable
+    {
+        return $this->shippedAt;
+    }
+
+    public function setShippedAt(DateTimeImmutable $shippedAt): static
+    {
+        $this->shippedAt = $shippedAt;
+
+        return $this;
+    }
+
+    public function getCompletedAt(): ?DateTimeImmutable
+    {
+        return $this->completedAt;
+    }
+
+    public function setCompletedAt(DateTimeImmutable $completedAt): static
+    {
+        $this->completedAt = $completedAt;
+
+        return $this;
+    }
+
+    public function getCanceledAt(): ?DateTimeImmutable
+    {
+        return $this->canceledAt;
+    }
+
+    public function setCanceledAt(DateTimeImmutable $canceledAt): static
+    {
+        $this->canceledAt = $canceledAt;
+
+        return $this;
+    }
+}
