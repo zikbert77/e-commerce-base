f0b5581fcb6492dfab86a001af3468013cf8fbb7
zikbert77 <zikbert77@gmail.com>
2026-03-20 14:39:48 +0100

    Fix

--- assets/admin.js
+++ assets/admin.js
@@ -1,4 +1,6 @@
 // Admin panel entry point — load only via importmap('admin') in admin base template
 import './bootstrap.js';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/admin.css';
